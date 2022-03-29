7276aea19041b66882b08770deddb0844c438d7c
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-03-30 06:47:23 +0800

    onboarding modal refactor

--- libs/shared/ui/src/lib/button/button.module.ts
+++ libs/shared/ui/src/lib/button/button.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { ButtonComponent } from './button.component';
 
 @NgModule({
-  imports: [CommonModule],
   declarations: [ButtonComponent],
   exports: [ButtonComponent],
+  imports: [CommonModule],
 })
 export class ButtonComponentModule {}
--- libs/shared/ui/src/lib/modal/modal.component.ts
+++ libs/shared/ui/src/lib/modal/modal.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'zombie-apocalypse-modal',
   templateUrl: './modal.component.html',
 })
-export class ModalComponent {}
+export class ModalComponent {
+  // add tailwind sizes where required
+  @Input() size: 'xl' | '2xl' = '2xl';
+}
